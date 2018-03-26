c767516f3ff9797f93873298b013d99081284ca5
MagnusGreiff <mangegreiff@gmail.com>
2018-03-26 19:13:19 +0200

    added more replace

--- src/js/database.js
+++ src/js/database.js
@@ -14,7 +14,9 @@ exports.dbGetAllData = async () => {
 };
 
 exports.dbInsertData = async (data) => {
-    data.text.replace(/\</gi, "%3C").replace(/\>/gi, "%3E").replace(/\=/gi, "%3D").replace(/\'/gi, "%27").replace(/\ /gi, "%20").replace(/\_/gi, "%5F").replace(/\//gi, "%2F").replace(/\-/gi, "%2D");
+
+    data.text.replace(/\</gi, "%3C").replace(/\>/gi, "%3E").replace(/\=/gi, "%3D").replace(/\'/gi, "%27").replace(/\ /gi, "%20").replace(/\_/gi, "%5F").replace(/\//gi, "%2F").replace(/\-/gi, "%2D").replace(/\:/gi, "%3A").replace(/\./gi, "%2E").replace(/\?/gi, "%3F");
+
     let newData = JSON.stringify(data);
 
     await fetch('http://localhost:3000/db/insert/' + newData);
